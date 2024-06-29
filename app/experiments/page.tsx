5c9c640055fe14de9750cbf829a51b285440f98c
Sidney Hopkins <sidhopkins14@gmail.com>
2024-06-29 18:27:40 -0400

    delete unused components

--- app/experiments/page.tsx
+++ app/experiments/page.tsx
@@ -1,24 +1,28 @@
-import ExperimentItem from '../components/ExperimentItem'
+import ExperimentItem from "../components/ExperimentItem";
 
 export default function Experiemnts() {
   return (
-    <section className='mx-auto max-w-5xl'>
-    <h2 className='portfolio-section__heading text-6xl font-bold pb-8'>Experiments</h2>
-    <p className='pb-16'>These are some of my best attempts to solve odd problems or create something special! Much of these are rough-and-ready proof of concepts. They're my favorite 🩵 🪄💡</p>
+    <section className="mx-auto max-w-5xl">
+      <h2 className="portfolio-section__heading text-6xl font-bold pb-8">
+        Experiments
+      </h2>
+      <p className="pb-16">
+        These are some of my best attempts to solve odd problems or create
+        something special! Much of these are rough-and-ready proof of concepts.
+        They're my favorite 🩵 🪄💡
+      </p>
 
-
-    <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
-        <ExperimentItem/>
-        <ExperimentItem/>
-        <ExperimentItem/>
-        <ExperimentItem/>
-        <ExperimentItem/>
-        <ExperimentItem/>
-        <ExperimentItem/>
-        <ExperimentItem/>
-        <ExperimentItem/>
-
-    </div>
-</section>
-  )
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        <ExperimentItem />
+        <ExperimentItem />
+        <ExperimentItem />
+        <ExperimentItem />
+        <ExperimentItem />
+        <ExperimentItem />
+        <ExperimentItem />
+        <ExperimentItem />
+        <ExperimentItem />
+      </div>
+    </section>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,66 @@
-
 export default function Home() {
-    return (
-
-      <section className="mx-auto max-w-5xl">
-        <div className="flex flex-col md:flex-row justify-between items-center pb-12 md:pb-24">
-          <div className="order-2 md:order-1 w-full md:w-2/3">
-            <h1 className="sr-only">Sidney's portfolio</h1>
-            <p className="pb-8 text-8xl font-bold">
-              Hi, I'm <span className="whitespace-nowrap">Sidney</span>
-            </p>
-            <p className="leading-9 max-w-xl text-3xl">
-              I'm a self-taught web developer who likes building front-ends,
-              bird watching, painting in watercolor, DnD, and playing
-              raquet sports.
-            </p>
-          </div>
-          <div className="order-1 md:order-2 aspect-square rounded-b-full w-5/6 md:w-1/3 md:mr-6">
-            <img
-              className="rounded-full object-cover aspect-square"
-              src="/img/hp.jpg"
-              alt=""
-            />
-          </div>
+  return (
+    <section className="mx-auto max-w-5xl">
+      <div className="flex flex-col md:flex-row justify-between items-center pb-12 md:pb-24">
+        <div className="order-2 md:order-1 w-full md:w-2/3">
+          <h1 className="sr-only">Sidney's portfolio</h1>
+          <p className="pb-8 text-8xl font-bold">
+            Hi, I'm <span className="whitespace-nowrap">Sidney</span>
+          </p>
+          <p className="leading-9 max-w-xl text-3xl">
+            I'm a self-taught web developer who likes building front-ends, bird
+            watching, painting in watercolor, DnD, and playing racquet sports.
+          </p>
         </div>
-        <h3 className="pb-6 font-bold text-4xl">Toolbox</h3>
-        <ul className="text-left md:max-w-3xl">
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            Git
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            HTML
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            CSS
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            JavaScript ES6
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            TypeScript
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            NextJS
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            React
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            Tailwind
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            SASS
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            Google Fonts
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            Figma
-          </li>
-          <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
-            Azure DevOps
-          </li>
-        </ul>
+        <div className="order-1 md:order-2 aspect-square rounded-b-full w-5/6 md:w-1/3 md:mr-6">
+          <img
+            className="rounded-full object-cover aspect-square"
+            src="/img/hp.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <h3 className="pb-6 font-bold text-4xl">Toolbox</h3>
+      <ul className="text-left md:max-w-3xl">
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          Git
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          HTML
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          CSS
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          JavaScript ES6
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          TypeScript
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          NextJS
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          React
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          Tailwind
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          SASS
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          Google Fonts
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          Figma
+        </li>
+        <li className="inline-block mr-3 my-2 px-3 py-2 rounded-3xl border border-black dark:border-white">
+          Azure DevOps
+        </li>
+      </ul>
 
-        <h2>Resume? CTA? More general info about my work. </h2>
-      </section>
-    )
+      <h2>Resume? CTA? More general info about my work. </h2>
+    </section>
+  );
 }
